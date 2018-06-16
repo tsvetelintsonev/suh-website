98a7a2f93eb4b0858d6a251ca90fdc30ed87ed09
Tsvetelin Tsonev <github.tosnev@yahoo.com>
2018-06-17 00:45:28 +0200

    set up general ui feel and look

--- ui/angular/src/app/app-routing/app-routing.module.ts
+++ ui/angular/src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { HomeComponent } from './../components/home/home.component';
+import { JobsComponent } from './../components/jobs/jobs.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Route } from '@angular/compiler/src/core';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent }
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'jobs', pathMatch: 'full', component: JobsComponent }
 ];
 
 @NgModule({
--- ui/angular/src/app/app.module.ts
+++ ui/angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { JobsComponent } from './components/jobs/jobs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule,
